e86636cf68ce169ede981970e907e9adb26cb2f0
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-10 16:40:12 -0400

    Update Hero component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,3 +1,24 @@
-function Hero() {}
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/logo.png"
+          alt="An image showing Omer"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Omer</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+      </p>
+    </section>
+  );
+}
 
 export default Hero;
