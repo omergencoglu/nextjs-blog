ea4be2bfa715e12e9ff55ce57646101a4981435b
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-10 17:59:05 -0400

    Add posts components

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,3 +1,11 @@
-function FeaturedPosts() {}
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}
 
 export default FeaturedPosts;
