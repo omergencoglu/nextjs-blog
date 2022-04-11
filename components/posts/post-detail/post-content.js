30174a37d3076fda67b7e8dab4db580a816162ea
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-10 22:01:45 -0400

    Add detail page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,24 @@
+import PostHeader from "./post-header";
+
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  date: "2022-02-10",
+  slug: "getting-started-with-nextjs",
+  content: "# This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
