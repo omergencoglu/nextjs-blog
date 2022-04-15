e05fc9a6cf76e6ff305b238dfda43e46f3a38f86
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-15 13:19:41 -0400

    Use markdown data

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,25 @@ import { Fragment } from "react";
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs2",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs3",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs4",
-  },
-];
-
-function HomePage() {
+function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,42 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs2",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs3",
-  },
-  {
-    title: "Getting Started with NextJS",
-    image: "getting-started-nextjs.png",
-    excerpt:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, pariatur!",
-    date: "2022-02-10",
-    slug: "getting-started-with-nextjs4",
-  },
-];
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts} />;
+}
 
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
 
 export default AllPostsPage;
